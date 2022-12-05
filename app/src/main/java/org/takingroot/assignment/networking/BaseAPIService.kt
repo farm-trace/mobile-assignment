1268c27661f1c84e360b15acceca3410c6d36d7d
Kaio Brito <kaio.brito92@gmail.com>
2022-12-05 09:20:19 -0300

    feat: Create BaseAPIService

---
+++ app/src/main/java/org/takingroot/assignment/networking/BaseAPIService.kt
@@ -0,0 +1,23 @@
+package org.takingroot.assignment.networking
+
+import org.takingroot.assignment.models.Survey
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface BaseAPIService {
+    @POST("survey/{type}")
+    suspend fun response(@Path("type") type: String, @Body survey: Survey): Response<Void>
+}
+
+class RetrofitInstance {
+    fun getInstance(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://assignments.takingroot.app")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
